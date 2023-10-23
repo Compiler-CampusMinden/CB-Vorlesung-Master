159634c532588a30153a648a7128a1872b35935c
Carsten Gips <cagix@fh-bielefeld.de>
2023-10-23 14:50:10 +0200

    reset antlr env

--- lecture/frontend/lexing/src/env.sh
+++ lecture/frontend/lexing/src/env.sh
@@ -1,4 +1,4 @@
-export CLASSPATH=".:antlr-4.13.1-complete.jar:$CLASSPATH"
+export CLASSPATH=".:antlr-4.11.1-complete.jar:$CLASSPATH"
 
 alias antlr='java org.antlr.v4.Tool'
 alias grun='java org.antlr.v4.gui.TestRig'
